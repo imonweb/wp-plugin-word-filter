a419ead139095b2e7d322f03d72490ef29899a39
Imon <contact@imonweb.co.uk>
2023-12-29 13:13:20 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Our Word Filter Plugin
+ * Plugin URI: https://github.com/imonweb/url
+ * Description: Replaces a list of words
+ * Author: Imon
+ * Author URI: https://www.imonweb.co.uk
+ * Text-Domain: Our Word Filter
+ * Version: 0.1.0
+ * License: GPL2
+ * License URL: https://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: our word filter
+ **/
+
+if( ! defined('ABSPATH')) exit; // Exit if accessed directly
